302c13c016d91df8e2074708fc25816d8ad83ba2
egg4096 <72076491+egg4096@users.noreply.github.com>
2020-10-10 14:40:48 +0300

    Update compiler.java

--- compiler.java
+++ compiler.java
@@ -2,38 +2,46 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.io.*;
 
-public class compiler{
+public class compiler {
     static String line;
     public static void main(String[] args) throws IOException {
-        File filein = new File("in.snail"); // The file we are reading/writing to
-        File fileout = new File("out.java");
+        File filein = new File("src/src/in.snail"); //input file
+        File fileout = new File("src/src/out.java"); //output file
         String storage;
         String storage2;
-        PrintWriter pw = new PrintWriter(new FileWriter(fileout, false)); // Change the true to false if you don't want to append, this is going to write to our file
-        BufferedReader br = new BufferedReader(new FileReader(filein)); // This is going to read from our file instead
-        int importi = 2;
+        PrintWriter pw = new PrintWriter(new FileWriter(fileout, false));
+        BufferedReader br = new BufferedReader(new FileReader(filein)); 
+        int legs;
+        String whitespaces = "";
         while((line = br.readLine()) != null){
-            if (importi == 2) importi = 1;
-            if (line.indexOf(',') != line.lastIndexOf(',') && line.charAt(0) == ',') {
-                line = line.substring(1);
-                storage = line.substring(0, line.lastIndexOf(','));
-                line = (storage2 = line.substring(line.lastIndexOf(',') + 2)) + " = " + storage;
-                if (storage2.equals("out")) line = "System.out.print(" + storage + ')';
-                else if (storage2.equals("libs")) {
-                    line = "import " + storage;
-                    importi = 2;
+            if (line.length() > 0) {
+                whitespaces = "";
+                if (line.charAt(0) == ' ') {
+                    legs = 0;
+                    while (line.charAt(legs) == ' ') {
+                        legs++;
+                    }
+                    whitespaces = line.substring(0, legs);
+                    line = line.substring(legs);
                 }
+                if (line.charAt(line.length() - 1) == ';') {
+                    if (line.equals(";")) line = "}";
+                    else line = line.substring(0, line.length() - 1) + " {";
+                }
+                if (line.indexOf(',') != line.lastIndexOf(',') && line.charAt(line.lastIndexOf(',') - 1) == ',') {
+                    storage = line.substring(0, line.lastIndexOf(',') - 1);
+                    line = (storage2 = line.substring(line.lastIndexOf(',') + 2)) + " = " + storage;
+                    if (storage2.equals("out")) line = "System.out.print(" + storage + ')';
+                    else if (storage2.equals("libs")) {
+                        line = "import " + storage;
+                    }
+                }
+                if (line.length() != 0 && "{};".indexOf(line.charAt(line.length() - 1)) == -1 && line.charAt(0) != '@') line = String.format("%s;", line);
             }
-            if (line.length() != 0 && "{};".indexOf(line.charAt(line.length() - 1)) == -1) line += ';';
-            if (importi == 1) {
-                line = "public class out {\n" + line;
-                importi = 8;
-            }
-            pw.println(line);
+            pw.println(whitespaces + line);
         }
-        pw.println("}");
         pw.close();
         pw.flush();
-        br.close(); // We have to close the BufferedReader to prevent memory leaks
+        br.close();
     }
 }
